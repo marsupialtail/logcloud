d3f948b4f94ff94a09bd826087cb949f34ad8cc3
Ziheng Wang <zihengw@stanford.edu>
2023-10-26 20:10:47 -0700

    updated install

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
+sudo apt update
+sudo apt install -y make cmake g++ unzip zlib1g-dev libssl-dev libzstd-dev libsnappy-dev liblz4-dev
+
+# first install the aws cli. I don't use it but you probably need it later to sync the index up to s3
+
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
-sudo apt install cmake
-sudo apt install g++
-sudo apt install unzip
-sudo apt install zlib1g-dev
-sudo apt install libssl-dev
-sudo apt install libzstd-dev libsnappy-dev liblz4-dev
+
+# install the AWS C++ SDK
 
 git clone https://github.com/aws/aws-sdk-cpp.git
 cd aws-sdk-cpp/
@@ -20,6 +21,8 @@ sudo make install
 cd ..
 cd ..
 
+# install libdivsufsort from source
+
 git clone https://github.com/y-256/libdivsufsort.git
 cd libdivsufsort
 mkdir build
@@ -29,3 +32,5 @@ make -j8
 sudo make install
 cd ..
 cd ..
+
+
